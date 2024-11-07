ceea81cb01bf928ca54eceec798d37cc31ad7a30
Evano-jay93 <evansmutisya93@gmail.com>
2024-11-07 21:46:02 +0300

    Add files via upload

---
+++ CAT 2 QUESTION 2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	int scores[2][4]= {{65,92,35,70},{84,72,59,67}};
+   for(a=0;a<2;a++){
+    	for(b=0;b<4;b++){
+    		printf("scores[%d][%d]-%d \n",i,j,scores[i][j]);
+		}
+    	
+    	
+    }
+    return 0;
+	
+}
+	 
+
+     
+	
+	
+	
+	
+
+
+
+
